3856399e28f6db9f1eda9089812cc3f1e2c5c052
Vitor Margis <vitor.margis@gmail.com>
2020-02-12 11:23:00 +0100

    Wed Feb 12 2020 11:23:00 GMT+0100 (Central European Standard Time)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var fs = require('fs');
+const fs = require('fs');
 const { exec } = require("child_process");
+const CronJob = require('cron').CronJob;
 
 const gitCommand = (msg) => {
     exec(`git add --all && git commit -am '${msg}' && git pull origin master && git push origin master`, (error, stdout, stderr) => {
@@ -15,6 +16,11 @@ const gitCommand = (msg) => {
     });    
 }
 
-fs.writeFile('date.txt', new Date(), function (err) {
-  gitCommand(new Date())
-});
+const job = new CronJob('*/1 * * * *', function() {
+  console.log('You will see this message every second');
+  fs.writeFile('date.txt', new Date(), function (err) {
+    gitCommand(new Date())
+  });
+}, null, true, 'America/Los_Angeles');
+job.start();
+
