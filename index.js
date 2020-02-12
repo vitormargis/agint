4811d5e46c21183153a662ac7ba2c55248147bae
Vitor Margis <vitor.margis@gmail.com>
2020-02-12 11:11:54 +0100

    Update

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+var fs = require('fs');
 const { exec } = require("child_process");
 
-exec(" git add --all && git commit -am 'Update' && git pull origin master && git push origin master", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
+const gitCommand = () => {
+    exec(" git add --all && git commit -am 'Update' && git pull origin master && git push origin master", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });    
+}
+
+fs.writeFile('date.txt', new Date(), function (err) {
+  gitCommand()
 });
